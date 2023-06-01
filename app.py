91241dd5113a8e66b37ca8b4ba807a8deac9978a
Rohit Bharmal <79892089+Rohit2601@users.noreply.github.com>
2023-06-01 18:59:26 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,47 @@
+# Importing essential libraries
+from flask import Flask, render_template, request, redirect, url_for
+import pickle
+import numpy as np
+
+# Load the Random Forest CLassifier model
+filename = 'git_model.pkl'
+model = pickle.load(open(filename, 'rb'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('frontpage.html')
+
+@app.route('/predict')
+def predict_page():
+    return render_template('predict.html')
+
+@app.route('/predict', methods=['GET','POST'])
+def predict():
+    
+    if request.method == 'POST':
+        age = int(request.form['age'])
+        sex = request.form.get('sex')
+        cp = request.form.get('cp')
+        trestbps = int(request.form['trestbps'])
+        chol = int(request.form['chol'])
+        fbs = request.form.get('fbs')
+        restecg = int(request.form['restecg'])
+        thalach = int(request.form['thalach'])
+        exang = request.form.get('exang')
+        oldpeak = float(request.form['oldpeak'])
+        slope = request.form.get('slope')
+        ca = int(request.form['ca'])
+        thal = request.form.get('thal')
+        
+        
+        data = np.array([[age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal]])
+        my_prediction = model.predict(data)
+        
+    return render_template('result.html', prediction=my_prediction)
+                
+
+if __name__ == '__main__':
+	app.run(debug=True)
+
